e50b9cee5e13a2df39473c9b2e8cdeaa42944ffc
Katherine <katherine@kryptnostic.com>
2016-08-30 18:35:43 -0700

    added stream serializers for fqn and entityset

---
+++ src/main/java/com/kryptnostic/conductor/rpc/serializers/FullQualifiedNameStreamSerializer.java
@@ -0,0 +1,41 @@
+package com.kryptnostic.conductor.rpc.serializers;
+
+import java.io.IOException;
+
+import org.apache.olingo.commons.api.edm.FullQualifiedName;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer;
+import com.kryptnostic.mapstores.v1.constants.HazelcastSerializerTypeIds;
+
+public class FullQualifiedNameStreamSerializer implements SelfRegisteringStreamSerializer<FullQualifiedName> {
+
+	@Override
+	public void write(ObjectDataOutput out, FullQualifiedName object)
+			throws IOException {
+		out.writeUTF( object.getName() );
+		out.writeUTF( object.getNamespace() );
+	}
+
+	@Override
+	public FullQualifiedName read(ObjectDataInput in) throws IOException {
+		return new FullQualifiedName( in.readUTF(), in.readUTF() );
+	}
+
+	@Override
+	public int getTypeId() {
+		return HazelcastSerializerTypeIds.FULL_QUALIFIED_NAME.ordinal();
+	}
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public Class<FullQualifiedName> getClazz() {
+		return FullQualifiedName.class;
+	}
+
+}
--- src/main/java/com/kryptnostic/conductor/rpc/serializers/QueryResultStreamSerializer.java
+++ src/main/java/com/kryptnostic/conductor/rpc/serializers/QueryResultStreamSerializer.java
@@ -1,88 +1,45 @@
 package com.kryptnostic.conductor.rpc.serializers;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.invoke.SerializedLambda;
-import java.util.UUID;
-import java.util.concurrent.Callable;
 
-import org.objenesis.strategy.StdInstantiatorStrategy;
+import javax.inject.Inject;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
-import com.esotericsoftware.kryo.serializers.ClosureSerializer;
+import com.google.common.base.Optional;
+
+import com.datastax.driver.core.Session;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.kryptnostic.conductor.rpc.Lambdas;
 import com.kryptnostic.conductor.rpc.QueryResult;
 import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer;
 import com.kryptnostic.services.v1.serialization.UUIDStreamSerializer;
 import com.kryptnostic.mapstores.v1.constants.HazelcastSerializerTypeIds;
 
-import org.apache.olingo.commons.api.edm.provider.CsdlEntitySet;
-import org.apache.olingo.commons.api.format.ContentType;
-import org.apache.olingo.server.api.OData;
-import org.apache.olingo.server.api.serializer.ODataSerializer;
-import org.apache.olingo.server.api.serializer.SerializerException;
-import org.apache.olingo.server.api.serializer.SerializerResult;
-
 public class QueryResultStreamSerializer implements SelfRegisteringStreamSerializer<QueryResult> {
-    private static final ThreadLocal<Kryo> kryoThreadLocal = new ThreadLocal<Kryo>() {
-
-        @Override
-        protected Kryo initialValue() {
-            Kryo kryo = new Kryo();
-            // Stuff from
-            // https://github.com/EsotericSoftware/kryo/blob/master/test/com/esotericsoftware/kryo/serializers/Java8ClosureSerializerTest.java
-            kryo.setInstantiatorStrategy( new Kryo.DefaultInstantiatorStrategy( new StdInstantiatorStrategy() ) );
-            kryo.register( Object[].class );
-            kryo.register( java.lang.Class.class );
-
-            // Shared Lambdas
-            kryo.register( Lambdas.class );
-            kryo.register( SerializedLambda.class );
-
-            // always needed for closure serialization, also if registrationRequired=false
-            kryo.register( ClosureSerializer.Closure.class, new ClosureSerializer() );
-
-            kryo.register( Callable.class, new ClosureSerializer() );
-
-            return kryo;
-        }
-    };
+	private final Session session;
+	
+	@Inject
+	public QueryResultStreamSerializer( Session session ) {
+		this.session = session;
+	}
         
 	@Override
 	public void write(ObjectDataOutput out, QueryResult object) throws IOException {
-		out.writeUTF( object.getTableName() );
 		out.writeUTF( object.getKeyspace() );
+		out.writeUTF( object.getTableName() );
 		UUIDStreamSerializer.serialize( out, object.getQueryId() );
 		out.writeUTF( object.getSessionId() );
-		// serialize Object.getEntitySet()
-		
-//		try {
-//			ODataSerializer od = OData.newInstance().createSerializer( ContentType.TEXT_PLAIN );
-//		//	SerializerResult result = od.entityCollection(object.getEntitySet()., entityType, object.getEntitySet(), options)
-//		} catch (SerializerException e) {
-//			e.printStackTrace();
-//		}
-		
-		Output output = new Output( (OutputStream) out );
-		kryoThreadLocal.get().writeClassAndObject(output, object);
+		new EntitySetStreamSerializer().write( out, object.getEntitySet() );	
 	}
 
 	@Override
 	public QueryResult read(ObjectDataInput in) throws IOException {
-		String tableName = in.readUTF();
-		String keyspace = in.readUTF();
-		UUID queryId = UUIDStreamSerializer.deserialize( in );
-		String sessionId = in.readUTF();
-		// deserialize entity set
-		CsdlEntitySet es = null; // remove this
-		Input input = new Input( (InputStream) in );
-		Callable<?> c = ( Callable<?> ) kryoThreadLocal.get().readClass( input );
-		return new QueryResult( tableName, keyspace, queryId, sessionId, es );
+		return new QueryResult(
+				in.readUTF(),
+				in.readUTF(),
+				UUIDStreamSerializer.deserialize( in ),
+				in.readUTF(),
+				new EntitySetStreamSerializer().read( in ),
+				Optional.fromNullable( session ) );
 	}
 
 	@Override
