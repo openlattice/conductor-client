ddcba2bef10a313753abe9f5c754b97bf6612051
Ho Chung Siu <hochung@kryptnostic.com>
2016-09-02 15:47:29 -0700

    added unit test for ResultSetConversion

--- src/main/java/com/kryptnostic/conductor/rpc/ResultSetConversion.java
+++ src/main/java/com/kryptnostic/conductor/rpc/ResultSetConversion.java
@@ -2,17 +2,14 @@ package com.kryptnostic.conductor.rpc;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
 
-import com.datastax.driver.core.Row;
 import com.datastax.driver.core.ColumnDefinitions;
-
-import com.google.common.collect.Multimap;
+import com.datastax.driver.core.Row;
+import com.google.common.base.Function;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
-import com.google.common.collect.Iterables;
 
 public final class ResultSetConversion {
 
@@ -23,7 +20,7 @@ public final class ResultSetConversion {
 	 * writes query result into Cassandra Table
 	 * 
 	 * @param mapTypenameToFullQualifiedName a Map that sends Typename to FullQualifiedName
-	 * @return a Function that converts Row to SetMultiMap, which has FullQualifiedName as a key and Row value as the corresponding value.
+	 * @return a Function that converts Row to SetMultimap, which has FullQualifiedName as a key and Row value as the corresponding value.
 	 */
 	public static Function< Row, SetMultimap<FullQualifiedName, Object> > toSetMultimap(
 			final Map<String, FullQualifiedName> mapTypenameToFullQualifiedName) {
