8cf6f4af342871fb86bad2b183e2f821d84c9f0b
Ho Chung Siu <hochung@kryptnostic.com>
2016-09-05 11:32:15 -0700

    BACKEND-492: use stream to replace for loop in ResultSetConversion.java; BACKEND-498: rename ResultSetConversion to ResultSetAdapterFactory.java

--- src/main/java/com/kryptnostic/conductor/rpc/ResultSetAdapterFactory.java
+++ src/main/java/com/kryptnostic/conductor/rpc/ResultSetAdapterFactory.java
@@ -2,6 +2,8 @@ package com.kryptnostic.conductor.rpc;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
 
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
 
@@ -9,9 +11,10 @@ import com.datastax.driver.core.ColumnDefinitions;
 import com.datastax.driver.core.Row;
 import com.google.common.base.Function;
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.SetMultimap;
 
-public final class ResultSetConversion {
+public final class ResultSetAdapterFactory {
 
 	private void ResultSetConversion() {}
 
@@ -24,17 +27,18 @@ public final class ResultSetConversion {
 	 */
 	public static Function< Row, SetMultimap<FullQualifiedName, Object> > toSetMultimap(
 			final Map<String, FullQualifiedName> mapTypenameToFullQualifiedName) {
-		return (Row row) -> {
-			List<ColumnDefinitions.Definition> definitions = row.getColumnDefinitions().asList();
-			int numOfColumns = definitions.size();
-			SetMultimap<FullQualifiedName, Object> convertedData = HashMultimap.create();
+		return (Row row) -> {			
+            List<ColumnDefinitions.Definition> definitions = row.getColumnDefinitions().asList();
+            int numOfColumns = definitions.size();
 
-			for (int i = 0; i < numOfColumns; i++) {
-				FullQualifiedName name = mapTypenameToFullQualifiedName.get(definitions.get(i).getName());
-				convertedData.put(name, row.getObject(i));
-			}
-
-			return convertedData;
+            return IntStream.range(0, numOfColumns).boxed().collect(
+			        Collector.of(
+			                ImmutableSetMultimap.Builder<FullQualifiedName, Object>::new,
+			                ( builder, index ) -> builder.put( mapTypenameToFullQualifiedName.get( definitions.get(index).getName() ) , row.getObject(index) ),
+			                ( lhs, rhs ) -> lhs.putAll( rhs.build() ),
+			                builder -> builder.build()
+			                )
+			        );
 		};
 	}
 }
