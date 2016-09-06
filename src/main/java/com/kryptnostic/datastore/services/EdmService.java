9383401478333075e3e82edecdae46aa109ab87d
Peng Hui How <peng@kryptnostic.com>
2016-09-06 14:55:22 -0700

    Re-included accidentally typename->user-friendly-FQName table calls (implemented in CassandraTableManager.java)

--- src/main/java/com/kryptnostic/datastore/services/EdmService.java
+++ src/main/java/com/kryptnostic/datastore/services/EdmService.java
@@ -152,6 +152,7 @@ public class EdmService implements EdmManager {
         String typename = tableManager.getTypenameForPropertyType( propertyType );
         propertyType.setTypename( typename );
         propertyTypeMapper.save( propertyType );
+        tableManager.updateFQNLookupTable( propertyType );
     }
 
     /*
@@ -186,6 +187,7 @@ public class EdmService implements EdmManager {
 
         if ( propertyCreated ) {
             tableManager.createPropertyTypeTable( propertyType );
+            tableManager.insertToFQNLookupTable( propertyType );
         }
 
         return propertyCreated;
@@ -207,6 +209,7 @@ public class EdmService implements EdmManager {
     @Override
     public void deletePropertyType( PropertyType propertyType ) {
         propertyTypeMapper.delete( propertyType );
+        tableManager.deleteFromFQNTable( propertyType );
     }
 
     @Override
