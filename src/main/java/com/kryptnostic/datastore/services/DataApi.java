00b0353f4454a4cca5f1b37c913c675b5445f747
Yao <yao@kryptnostic.com>
2016-09-21 12:10:59 -0700

    rename

--- src/main/java/com/kryptnostic/datastore/services/DataApi.java
+++ src/main/java/com/kryptnostic/datastore/services/DataApi.java
@@ -4,21 +4,23 @@ import com.google.common.collect.SetMultimap;
 import com.kryptnostic.conductor.rpc.*;
 import com.squareup.okhttp.Response;
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
-import retrofit.http.GET;
-import retrofit.http.POST;
+import retrofit.http.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 /**
  * Created by yao on 9/20/16.
  */
-public interface RawDataStorageApi {
-    String CONTROLLER  = "／raw";
+public interface DataApi {
+    String CONTROLLER = "/raw";
 
     String ENTITYSET   = "/entityset";
     String ENTITY_DATA = "/entitydata";
-    String FILTERED    = "filtered";
+    String FILTERED    = "/filtered";
+    String INTEGRATION = "/integration";
 
     @GET( CONTROLLER + ENTITYSET )
     List<UUID> getAllEntitySet( LoadEntitySetRequest loadEntitySetRequest );
@@ -34,5 +36,14 @@ public interface RawDataStorageApi {
 
     @POST( CONTROLLER + ENTITY_DATA )
     Response createEntityData( CreateEntityRequest createEntityRequest );
+
+    @GET( CONTROLLER + INTEGRATION )
+    Map<String, String> getAllIntegrationScripts();
+
+    @PUT( CONTROLLER + INTEGRATION )
+    Map<String, String> getIntegrationScript( @Body Set<String> url );
+
+    @POST( CONTROLLER + INTEGRATION )
+    Response createIntegrationScript( @Body Map<String, String> integrationScripts );
 
 }
