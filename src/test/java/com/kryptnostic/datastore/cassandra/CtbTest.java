3f9deabf77e1959930aa21cda4dca7c4984a9eb6
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2016-11-21 15:17:52 -0800

    Move CTB into rhizome

--- src/test/java/com/kryptnostic/datastore/cassandra/CtbTest.java
+++ src/test/java/com/kryptnostic/datastore/cassandra/CtbTest.java
@@ -3,12 +3,13 @@ package com.kryptnostic.datastore.cassandra;
 import org.junit.Test;
 
 import com.kryptnostic.conductor.rpc.odata.Tables;
+import com.kryptnostic.rhizome.cassandra.CassandraTableBuilder;
 
 public class CtbTest {
 
     @Test
     public void testTableQuery() {
-        System.out.println( new CassandraTableBuilder( Tables.ENTITY_SETS.getTableName() ).ifNotExists()
+        System.out.println( new CassandraTableBuilder( Tables.ENTITY_SETS.getName() ).ifNotExists()
                 .partitionKey( CommonColumns.TYPE ,CommonColumns.ACLID )
                 .clusteringColumns( CommonColumns.NAME )
                 .columns( CommonColumns.TITLE ).buildQuery() );
