39ea41c87a4382fa527bc1cc7539c5bf74739837
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-07 01:09:44 -0800

    Fix compile

--- src/main/java/com/dataloom/hazelcast/MapstoresPod.java
+++ src/main/java/com/dataloom/hazelcast/MapstoresPod.java
@@ -11,10 +11,7 @@ import org.springframework.context.annotation.Import;
 import com.dataloom.authorization.AceKey;
 import com.dataloom.authorization.mapstores.PermissionMapstore;
 import com.dataloom.authorization.requests.Permission;
-import com.dataloom.edm.internal.DatastoreConstants;
 import com.datastax.driver.core.Session;
-import com.kryptnostic.datastore.cassandra.CommonColumns;
-import com.kryptnostic.rhizome.cassandra.CassandraTableBuilder;
 import com.kryptnostic.rhizome.mapstores.SelfRegisteringMapStore;
 import com.kryptnostic.rhizome.pods.CassandraPod;
 
@@ -27,12 +24,6 @@ public class MapstoresPod {
 
     @Bean
     public SelfRegisteringMapStore<AceKey, EnumSet<Permission>> permissionMapstore() {
-        return new PermissionMapstore(
-                session,
-                new CassandraTableBuilder( DatastoreConstants.KEYSPACE, PermissionMapstore.MAP_NAME )
-                        .ifNotExists()
-                        .partitionKey( CommonColumns.ACL_KEYS )
-                        .clusteringColumns( CommonColumns.PRINCIPAL_TYPE, CommonColumns.PRINCIPAL_ID )
-                        .columns( CommonColumns.SECURABLE_OBJECT_TYPE, CommonColumns.PERMISSIONS ) );
+        return new PermissionMapstore( session );
     }
 }
