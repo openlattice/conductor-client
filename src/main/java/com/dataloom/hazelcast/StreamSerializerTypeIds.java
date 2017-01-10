e88daf82c47d6dca3030935d01cc314023059fd2
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-10 14:54:13 -0800

    Added placeholder and warning

--- src/main/java/com/dataloom/hazelcast/StreamSerializerTypeIds.java
+++ src/main/java/com/dataloom/hazelcast/StreamSerializerTypeIds.java
@@ -1,6 +1,16 @@
 package com.dataloom.hazelcast;
 
+/**
+ * Simplifies management of type ids within Hazelcast for serialization. Can be re-ordered safely unless doing hot upgrade.
+ * <b>NOTE: Leave first entry in place</b> 
+ * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt; 
+ *
+ */
 public enum StreamSerializerTypeIds {
+    /**
+     * Move this one, break everything.
+     */
+    STREAM_SERIALIZER_IDS_MUST_BE_POSTIVE,
     RUNNABLE,
     CALLABLE,
     CONDUCTOR_CALL,
