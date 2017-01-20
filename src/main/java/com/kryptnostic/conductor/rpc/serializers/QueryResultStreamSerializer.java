61c35a1cec32a7954ebb9bffed368cf4e8d6e94e
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-19 21:32:14 -0800

    Woops

--- src/main/java/com/kryptnostic/conductor/rpc/serializers/QueryResultStreamSerializer.java
+++ src/main/java/com/kryptnostic/conductor/rpc/serializers/QueryResultStreamSerializer.java
@@ -3,6 +3,8 @@ package com.kryptnostic.conductor.rpc.serializers;
 import java.io.IOException;
 import java.util.UUID;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Component;
 
 import com.dataloom.hazelcast.StreamSerializerTypeIds;
@@ -16,52 +18,7 @@ import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer;
 
 @Component
 public class QueryResultStreamSerializer implements SelfRegisteringStreamSerializer<QueryResult> {
-<<<<<<< HEAD
 	private Session session;
-	
-	public QueryResultStreamSerializer( Session session ) {
-		this.session = session;
-	}
-        
-	@Override
-	public void write(ObjectDataOutput out, QueryResult object) throws IOException {
-		out.writeUTF( object.getKeyspace() );
-		out.writeUTF( object.getTableName() );
-		AbstractUUIDStreamSerializer.serialize( out, object.getQueryId() );
-		out.writeUTF( object.getSessionId() );
-	}
-
-	@Override
-	public QueryResult read(ObjectDataInput in) throws IOException {
-		String keyspace = in.readUTF();
-		String tableName = in.readUTF();
-		UUID queryId = AbstractUUIDStreamSerializer.deserialize( in );
-		String sessionId = in.readUTF();
-		return new QueryResult( keyspace, tableName, queryId, sessionId, Optional.fromNullable( session ) );
-	}
-
-	@Override
-	public int getTypeId() {
-		return StreamSerializerTypeIds.QUERY_RESULT.ordinal();
-	}
-
-	@Override
-	public void destroy() {
-		
-	}
-
-	public synchronized void setSession( Session session ) {
-		if( this.session == null ) {
-			this.session = Preconditions.checkNotNull( session );
-		}
-	}
-
-	@Override
-	public Class<QueryResult> getClazz() {
-		return QueryResult.class;
-	}
-=======
-    private Session session;
 
     @Inject
     public QueryResultStreamSerializer( Session session ) {
@@ -105,6 +62,5 @@ public class QueryResultStreamSerializer implements SelfRegisteringStreamSeriali
     public Class<QueryResult> getClazz() {
         return QueryResult.class;
     }
->>>>>>> develop
 
 }
