53b73d1342484ddd2f242f0aaeb0fbd72d6596f8
Ho Chung Siu <hochung@kryptnostic.com>
2017-01-23 19:05:21 -0800

    update permissions when requests are approved; switching to executeOnKey temporarily

--- src/main/java/com/dataloom/requests/HazelcastRequestsManager.java
+++ src/main/java/com/dataloom/requests/HazelcastRequestsManager.java
@@ -39,10 +39,10 @@ public class HazelcastRequestsManager {
     }
 
     public void submitAll( Map<AceKey, Status> statusMap ) {
-        Map<AceKey, DelegatedPermissionEnumSet> permissionToAdd = statusMap.entrySet().stream()
+        statusMap.entrySet().stream()
                 .filter( e -> e.getValue().getStatus().equals( RequestStatus.APPROVED ) )
-                .forEach( e -> aces.submitToKey( e.getKey(),
-                        new PermissionMerger( DelegatedPermissionEnumSet.wrap( e.getValue().getPermissions() ) ) );
+                .map( e -> aces.executeOnKey( e.getKey(),
+                        new PermissionMerger( e.getValue().getPermissions() ) ) );
         requests.putAll( statusMap );
     }
 
