004bc5c051558424abbab79b01b4d969afef0e20
Ho Chung Siu <hochung@kryptnostic.com>
2017-01-23 19:52:28 -0800

    stream not ending

--- src/main/java/com/dataloom/requests/HazelcastRequestsManager.java
+++ src/main/java/com/dataloom/requests/HazelcastRequestsManager.java
@@ -39,7 +39,7 @@ public class HazelcastRequestsManager {
     public void submitAll( Map<AceKey, Status> statusMap ) {
         statusMap.entrySet().stream()
                 .filter( e -> e.getValue().getStatus().equals( RequestStatus.APPROVED ) )
-                .map( e -> aces.executeOnKey( e.getKey(),
+                .forEach( e -> aces.executeOnKey( e.getKey(),
                         new PermissionMerger( e.getValue().getPermissions() ) ) );
         requests.putAll( statusMap );
     }
