9e0f8a7330b34eabf3d5c16e1be55fa53e318846
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-24 14:27:02 -0700

    Fix permission merger

--- src/main/java/com/kryptnostic/conductor/rpc/serializers/PermissionRemoverStreamSerializer.java
+++ src/main/java/com/kryptnostic/conductor/rpc/serializers/PermissionRemoverStreamSerializer.java
@@ -1,6 +1,7 @@
 package com.kryptnostic.conductor.rpc.serializers;
 
 import com.dataloom.authorization.DelegatedPermissionEnumSet;
+import com.dataloom.authorization.Permission;
 import com.dataloom.authorization.processors.PermissionMerger;
 import com.dataloom.authorization.processors.PermissionRemover;
 import com.dataloom.hazelcast.StreamSerializerTypeIds;
@@ -10,25 +11,40 @@ import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.BitSet;
+import java.util.EnumSet;
 
 /**
  * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
  */
 @Component
 public class PermissionRemoverStreamSerializer implements SelfRegisteringStreamSerializer<PermissionRemover> {
+    private static final Permission[] P = Permission.values();
     @Override public Class<? extends PermissionRemover> getClazz() {
         return PermissionRemover.class;
     }
 
     @Override public void write(
             ObjectDataOutput out, PermissionRemover object ) throws IOException {
-        DelegatedPermissionEnumSet s = (DelegatedPermissionEnumSet) object.getBackingCollection();
-        DelegatedPermissionEnumSetStreamSerializer.serialize( out, s.unwrap() );
+        BitSet bs = new BitSet( P.length );
+        for ( Permission p : object.getBackingCollection() ) {
+            bs.set( p.ordinal() );
+        }
+        out.writeLongArray( bs.toLongArray() );
 
     }
 
     @Override public PermissionRemover read( ObjectDataInput in ) throws IOException {
-        return new PermissionRemover( DelegatedPermissionEnumSetStreamSerializer.deserialize( in ) );
+        BitSet bs = BitSet.valueOf( in.readLongArray() );
+
+        EnumSet<Permission> ps = EnumSet.noneOf( Permission.class );
+        for ( int i = 0; i < P.length; ++i ) {
+            if ( bs.get( i ) ) {
+                ps.add( P[ i ] );
+            }
+        }
+
+        return new PermissionRemover( ps );
     }
 
     @Override public int getTypeId() {
