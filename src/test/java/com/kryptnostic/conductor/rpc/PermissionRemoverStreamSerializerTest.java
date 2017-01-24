cd039cff69691e1701da22ba3b9e5717d96f360d
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-23 21:34:53 -0800

    Added tests and remover stream serializer

---
+++ src/test/java/com/kryptnostic/conductor/rpc/PermissionRemoverStreamSerializerTest.java
@@ -0,0 +1,22 @@
+package com.kryptnostic.conductor.rpc;
+
+import com.dataloom.authorization.DelegatedPermissionEnumSet;
+import com.dataloom.authorization.processors.PermissionMerger;
+import com.dataloom.authorization.processors.PermissionRemover;
+import com.dataloom.mapstores.TestDataFactory;
+import com.kryptnostic.conductor.rpc.serializers.PermissionRemoverStreamSerializer;
+import com.kryptnostic.rhizome.hazelcast.serializers.BaseSerializerTest;
+
+/**
+ * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
+ */
+public class PermissionRemoverStreamSerializerTest
+        extends BaseSerializerTest<PermissionRemoverStreamSerializer, PermissionRemover> {
+    @Override protected PermissionRemoverStreamSerializer createSerializer() {
+        return new PermissionRemoverStreamSerializer();
+    }
+
+    @Override protected PermissionMerger createInput() {
+        return new PermissionMerger( DelegatedPermissionEnumSet.wrap( TestDataFactory.permissions() ) );
+    }
+}
