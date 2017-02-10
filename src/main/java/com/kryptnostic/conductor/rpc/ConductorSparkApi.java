b540ef708380fc1db03cca2722ebef56385ae4a9
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-02-09 17:03:02 -0800

    Get all user pages

--- src/main/java/com/kryptnostic/conductor/rpc/ConductorSparkApi.java
+++ src/main/java/com/kryptnostic/conductor/rpc/ConductorSparkApi.java
@@ -37,10 +37,10 @@ public interface ConductorSparkApi {
             List<PropertyType> authorizedProperties );
 
     /**
-     * Return QueryResult of <b>UUID's ONLY</b> of all entities matching a Look Up Entities Request.
+     * Return QueryResult of <b>UUID's ONLY</b> of all entities linking a Look Up Entities Request.
      * 
      * @param request A LookupEntitiesRequest object
-     * @return QueryResult of UUID's matching the lookup request
+     * @return QueryResult of UUID's linking the lookup request
      */
     QueryResult getFilterEntities( LookupEntitiesRequest request );
 
