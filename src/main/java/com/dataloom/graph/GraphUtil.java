480ddfd6dcb3d2468542d7a856169e8cb4d2bd52
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-02-14 00:34:46 -0800

    Cleanup

--- src/main/java/com/dataloom/graph/GraphUtil.java
+++ src/main/java/com/dataloom/graph/GraphUtil.java
@@ -22,17 +22,13 @@ package com.dataloom.graph;
 
 import java.util.UUID;
 
-
-import com.dataloom.data.EntityKey;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
-import com.kryptnostic.datastore.cassandra.CommonColumns;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.dataloom.data.EntityKey;
 import com.dataloom.linking.util.UnorderedPair;
+import com.datastax.driver.core.Row;
+import com.kryptnostic.datastore.cassandra.CommonColumns;
 
 /**
  * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
