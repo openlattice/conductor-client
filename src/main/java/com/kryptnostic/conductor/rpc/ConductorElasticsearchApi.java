77c04873179f13a285866f3cb0f6ad1f15a756f6
Katherine <katherine@kryptnostic.com>
2017-02-16 00:49:17 -0800

    creating SearchResult object and serializer

--- src/main/java/com/kryptnostic/conductor/rpc/ConductorElasticsearchApi.java
+++ src/main/java/com/kryptnostic/conductor/rpc/ConductorElasticsearchApi.java
@@ -29,6 +29,7 @@ import com.dataloom.authorization.Principal;
 import com.dataloom.edm.EntitySet;
 import com.dataloom.edm.type.PropertyType;
 import com.dataloom.organization.Organization;
+import com.dataloom.search.requests.SearchResult;
 import com.google.common.base.Optional;
 
 public interface ConductorElasticsearchApi {
@@ -101,7 +102,7 @@ public interface ConductorElasticsearchApi {
 		
 	Boolean createEntityData( UUID entitySetId, String entityId, Map<UUID, String> propertyValues );
 	    
-    List<Map<String, Object>> executeEntitySetDataSearch( UUID entitySetId, String searchTerm, Set<UUID> authorizedPropertyTypes );
+	SearchResult executeEntitySetDataSearch( UUID entitySetId, String searchTerm, int start, int maxHits, Set<UUID> authorizedPropertyTypes );
 	
     List<Map<String, Object>> executeEntitySetDataSearchAcrossIndices( Set<UUID> entitySetIds, Map<UUID, Set<String>> fieldSearches, int size, boolean explain );
 }
