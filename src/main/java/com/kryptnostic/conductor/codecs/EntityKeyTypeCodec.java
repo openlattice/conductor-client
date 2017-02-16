bd932ce31435686b69bc7905406447a6e7847222
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-02-16 01:44:12 -0800

    Optimize typecodecs

--- src/main/java/com/kryptnostic/conductor/codecs/EntityKeyTypeCodec.java
+++ src/main/java/com/kryptnostic/conductor/codecs/EntityKeyTypeCodec.java
@@ -1,27 +1,25 @@
 package com.kryptnostic.conductor.codecs;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.dataloom.data.EntityKey;
 import com.datastax.driver.core.DataType;
 import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.TypeCodec;
 import com.datastax.driver.core.exceptions.InvalidTypeException;
-import com.datastax.driver.core.utils.Bytes;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 public class EntityKeyTypeCodec extends TypeCodec<EntityKey> {
-    
-    private static final Logger     logger = LoggerFactory
+
+    private static final Logger logger = LoggerFactory
             .getLogger( EntityKeyTypeCodec.class );
 
     private final ObjectMapper mapper;
-    
+
     public EntityKeyTypeCodec( ObjectMapper mapper ) {
         super( DataType.blob(), EntityKey.class );
         this.mapper = mapper;
@@ -47,24 +45,31 @@ public class EntityKeyTypeCodec extends TypeCodec<EntityKey> {
         return TypeCodec.blob().format( serialize( value ) );
     }
 
-    private ByteBuffer serialize( EntityKey value ) throws InvalidTypeException { 
-        if( value == null ) return null;
-        try {
-            return ByteBuffer.wrap( mapper.writeValueAsBytes( value ) );
-        } catch ( JsonProcessingException e ) {
-            logger.error( "Failed to serialize entity key: " + value );
+    private ByteBuffer serialize( EntityKey value ) throws InvalidTypeException {
+        if ( value == null ) {
             return null;
         }
+        final byte[] idBytes = value.getEntityId().getBytes();
+        final int len = 2 * Long.BYTES + idBytes.length;
+        final byte[] bytes = new byte[ len ];
+        final ByteBuffer buf = ByteBuffer.wrap( bytes );
+        buf.putLong( value.getEntitySetId().getLeastSignificantBits() );
+        buf.putLong( value.getEntitySetId().getMostSignificantBits() );
+        buf.put( idBytes );
+        return buf.duplicate();
     }
-    
+
     private EntityKey deserialize( ByteBuffer bytes ) throws InvalidTypeException {
-        if( bytes == null ) return null;
-        try {
-            return mapper.readValue( Bytes.getArray( bytes ), EntityKey.class );
-        } catch ( IOException e ) {
-            logger.error( "Failed to deserialize entity key." );
+        if ( bytes == null ) {
             return null;
         }
+        ByteBuffer buf = bytes.duplicate();
+        long lsb = bytes.getLong();
+        long msb = bytes.getLong();
+        UUID entitySetId = new UUID( msb, lsb );
+        byte[] idBytes = new byte[ buf.remaining() ];
+        String entityId = new String( idBytes, StandardCharsets.UTF_8 );
+        return new EntityKey( entitySetId, entityId );
     }
 
 }
