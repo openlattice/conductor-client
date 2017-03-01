a605d104a53ef030261356749f1c49082496662e
Ho Chung Siu <hochung@kryptnostic.com>
2017-03-01 00:34:44 -0800

    add entry processor that update contacts field of entity set

---
+++ src/main/java/com/dataloom/edm/types/processors/UpdateEntitySetContactsProcessor.java
@@ -0,0 +1,39 @@
+package com.dataloom.edm.types.processors;
+
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.UUID;
+
+import com.dataloom.edm.EntitySet;
+import com.google.common.base.Optional;
+import com.kryptnostic.rhizome.hazelcast.processors.AbstractRhizomeEntryProcessor;
+
+public class UpdateEntitySetContactsProcessor extends AbstractRhizomeEntryProcessor<UUID, EntitySet, Object> {
+    private static final long serialVersionUID = 4846226537808942435L;
+    private final Set<String> contacts;
+
+    public UpdateEntitySetContactsProcessor( Set<String> contacts ) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public Object process( Entry<UUID, EntitySet> entry ) {
+        EntitySet es = entry.getValue();
+        if ( es != null ) {
+            EntitySet newEs = new EntitySet(
+                    es.getId(),
+                    es.getEntityTypeId(),
+                    es.getName(),
+                    es.getTitle(),
+                    Optional.of( es.getDescription() ),
+                    contacts );
+            entry.setValue( newEs );
+        }
+        return null;
+    }
+
+    public Set<String> getContacts() {
+        return contacts;
+    }
+
+}
