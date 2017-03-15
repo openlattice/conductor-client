d03f4a8b119562234592b4ce38be47d0025f5d37
Ho Chung Siu <hochung@kryptnostic.com>
2017-03-14 18:15:41 -0700

    add stream serializer tests

---
+++ src/test/java/com/dataloom/hazelcast/serializers/UpdateEntityTypeMetadataProcessorStreamSerializerTest.java
@@ -0,0 +1,34 @@
+package com.dataloom.hazelcast.serializers;
+
+import java.io.Serializable;
+
+import com.dataloom.edm.requests.MetadataUpdate;
+import com.dataloom.edm.type.EntityType;
+import com.dataloom.edm.types.processors.UpdateEntityTypeMetadataProcessor;
+import com.dataloom.mapstores.TestDataFactory;
+import com.google.common.base.Optional;
+import com.kryptnostic.rhizome.hazelcast.serializers.AbstractStreamSerializerTest;
+
+public class UpdateEntityTypeMetadataProcessorStreamSerializerTest extends
+        AbstractStreamSerializerTest<UpdateEntityTypeMetadataProcessorStreamSerializer, UpdateEntityTypeMetadataProcessor>
+        implements Serializable {
+    private static final long serialVersionUID = 6256754971472117558L;
+
+    @Override
+    protected UpdateEntityTypeMetadataProcessorStreamSerializer createSerializer() {
+        return new UpdateEntityTypeMetadataProcessorStreamSerializer();
+    }
+
+    @Override
+    protected UpdateEntityTypeMetadataProcessor createInput() {
+        EntityType et = TestDataFactory.entityType();
+        MetadataUpdate update = new MetadataUpdate(
+                Optional.of( et.getTitle() ),
+                Optional.of( et.getDescription() ),
+                Optional.absent(),
+                Optional.absent(),
+                Optional.of( et.getType() ) );
+        return new UpdateEntityTypeMetadataProcessor( update );
+    }
+
+}
