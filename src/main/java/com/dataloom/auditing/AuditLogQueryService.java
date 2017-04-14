52094b1a8ebcd2908ff2b589d309bb8bbed200da
Hristo Oskov <hristo@kryptnostic.com>
2017-04-14 13:41:44 -0700

    implement audit log store statement

--- src/main/java/com/dataloom/auditing/AuditLogQueryService.java
+++ src/main/java/com/dataloom/auditing/AuditLogQueryService.java
@@ -1,9 +1,23 @@
 package com.dataloom.auditing;
 
+import java.util.UUID;
+
+import com.dataloom.authorization.PrincipalType;
 import com.dataloom.neuron.AuditableSignal;
+import com.dataloom.neuron.SignalType;
+import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Session;
 import com.kryptnostic.conductor.rpc.odata.Table;
+
+import static com.kryptnostic.datastore.cassandra.CommonColumns.ACL_KEYS;
+import static com.kryptnostic.datastore.cassandra.CommonColumns.AUDIT_ID;
+import static com.kryptnostic.datastore.cassandra.CommonColumns.BLOCK_ID;
+import static com.kryptnostic.datastore.cassandra.CommonColumns.ENTITY_ID;
+import static com.kryptnostic.datastore.cassandra.CommonColumns.EVENT_TYPE;
+import static com.kryptnostic.datastore.cassandra.CommonColumns.PRINCIPAL_ID;
+import static com.kryptnostic.datastore.cassandra.CommonColumns.PRINCIPAL_TYPE;
+import static com.kryptnostic.datastore.cassandra.CommonColumns.TIME_UUID;
 
 public class AuditLogQueryService {
 
@@ -18,6 +32,16 @@ public class AuditLogQueryService {
 
     public void store( AuditableSignal signal ) {
 
-        throw new UnsupportedOperationException( "NOT IMPLEMENTED!" );
+        BoundStatement storeStatement = storeQuery.bind()
+                .setList( ACL_KEYS.cql(), signal.getAclKey(), UUID.class )
+                .set( EVENT_TYPE.cql(), signal.getType(), SignalType.class )
+                .set( PRINCIPAL_TYPE.cql(), signal.getPrincipal().getType(), PrincipalType.class )
+                .setString( PRINCIPAL_ID.cql(), signal.getPrincipal().getId() )
+                .setUUID( TIME_UUID.cql(), signal.getTimeId() )
+                .setUUID( ENTITY_ID.cql(), signal.getEntityId() )
+                .setUUID( AUDIT_ID.cql(), signal.getAuditId() )
+                .setUUID( BLOCK_ID.cql(), signal.getBlockId() );
+
+        session.executeAsync( storeStatement );
     }
 }
--- src/main/java/com/dataloom/auditing/HazelcastAuditLogService.java
+++ src/main/java/com/dataloom/auditing/HazelcastAuditLogService.java
@@ -3,20 +3,22 @@ package com.dataloom.auditing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dataloom.hazelcast.HazelcastMap;
+import com.dataloom.neuron.AuditableSignal;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
 
 public class HazelcastAuditLogService {
 
     private static final Logger logger = LoggerFactory.getLogger( HazelcastAuditLogService.class );
 
-    private final IMap<AuditableEventKey, AuditableEvent> auditLog;
     private final AuditLogQueryService                    auditLogQueryService;
 
     public HazelcastAuditLogService( HazelcastInstance hazelcastInstance, AuditLogQueryService auditLogQueryService ) {
 
-        this.auditLog = hazelcastInstance.getMap( HazelcastMap.AUDIT_LOG.name() );
         this.auditLogQueryService = auditLogQueryService;
+    }
+
+    public void log( AuditableSignal signal ) {
+
+        this.auditLogQueryService.store( signal );
     }
 }
