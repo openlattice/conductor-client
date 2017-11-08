13bec13bf9a5d760ea4f8513b698597a8007f69a
Katherine Bernstein <katherine@openlattice.com>
2017-11-07 17:37:34 -0800

    alphabetize columns, tables

--- src/main/java/com/openlattice/postgres/PostgresTable.java
+++ src/main/java/com/openlattice/postgres/PostgresTable.java
@@ -26,33 +26,6 @@ import static com.openlattice.postgres.PostgresColumn.*;
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 public final class PostgresTable {
-    public static PostgresTableDefinition SECURABLE_OBJECTS =
-            new PostgresTableDefinition( "securable_objects" )
-                    .addColumns( ACL_KEY, SECURABLE_OBJECT_TYPE )
-                    .primaryKey( ACL_KEY );
-
-    public static PostgresTableDefinition PERMISSIONS =
-            new PostgresTableDefinition( "permissions" )
-                    .addColumns( ACL_KEY, PRINCIPAL_TYPE, PRINCIPAL_ID, PostgresColumn.PERMISSIONS )
-                    .primaryKey( ACL_KEY, PRINCIPAL_TYPE, PRINCIPAL_ID );
-
-    //                case PERMISSIONS:
-    // TODO: Once Cassandra fixes SASI + Collection column inde
-    //            return new CassandraTableBuilder( PERMISSIONS )
-    //                        .ifNotExists()
-    //                        .partitionKey( CommonColumns.ACL_KEYS )
-    //                        .clusteringColumns( PRINCIPAL_TYPE, PRINCIPAL_ID )
-    //                        .columns( CommonColumns.PERMISSIONS )
-    //                        .staticColumns( SECURABLE_OBJECT_TYPE )
-    //                        .secondaryIndex( PRINCIPAL_TYPE,
-    //            PRINCIPAL_ID,
-    //            CommonColumns.PERMISSIONS,
-    //            SECURABLE_OBJECT_TYPE );
-
-    public static PostgresTableDefinition PROPERTY_TYPES =
-            new PostgresTableDefinition( "property_types" )
-                    .addColumns( ID, NAMESPACE, NAME, DATATYPE, TITLE, DESCRIPTION, SCHEMAS, PII, ANALYZER )
-                    .setUnique( NAMESPACE, NAME );
 
     public static PostgresTableDefinition ACL_KEYS =
             new PostgresTableDefinition( "acl_keys" )
@@ -60,10 +33,43 @@ public final class PostgresTable {
                     .primaryKey( NAME )
                     .setUnique( NAME );
 
-    public static PostgresTableDefinition NAMES =
-            new PostgresTableDefinition( "names" )
-                    .addColumns( SECURABLE_OBJECTID, NAME )
-                    .primaryKey( SECURABLE_OBJECTID );
+    public static PostgresTableDefinition ASSOCIATION_TYPES =
+            new PostgresTableDefinition( "association_types" )
+                    .addColumns( ID, SRC, DST, BIDIRECTIONAL );
+
+    public static PostgresTableDefinition AUDIT_LOG =
+            new PostgresTableDefinition( "audit_log" )
+                    .addColumns( ACL_KEY,
+                            EVENT_TYPE,
+                            PRINCIPAL_TYPE,
+                            PRINCIPAL_ID,
+                            TIME_UUID,
+                            AUDIT_ID,
+                            DATA_ID,
+                            BLOCK_ID )
+                    .primaryKey( ACL_KEY, EVENT_TYPE, PRINCIPAL_TYPE, PRINCIPAL_ID, TIME_UUID )
+                    .setUnique( ACL_KEY, EVENT_TYPE, PRINCIPAL_TYPE, PRINCIPAL_ID, TIME_UUID );
+
+    public static PostgresTableDefinition COMPLEX_TYPES =
+            new PostgresTableDefinition( "complex_types" )
+                    .addColumns( ID, NAMESPACE, NAME, TITLE, DESCRIPTION, PROPERTIES, BASE_TYPE, SCHEMAS, CATEGORY )
+                    .setUnique( NAMESPACE, NAME );
+
+    public static PostgresTableDefinition EDM_VERSIONS =
+            new PostgresTableDefinition( "edm_versions" )
+                    .addColumns( EDM_VERSION_NAME, EDM_VERSION )
+                    .primaryKey( EDM_VERSION_NAME, EDM_VERSION )
+                    .setUnique( EDM_VERSION_NAME, EDM_VERSION );
+
+    public static PostgresTableDefinition ENTITY_SET_PROPERTY_METADATA =
+            new PostgresTableDefinition( "entity_set_property_metadata" )
+                    .addColumns( ENTITY_SET_ID, PROPERTY_TYPE_ID, TITLE, DESCRIPTION, SHOW )
+                    .primaryKey( ENTITY_SET_ID, PROPERTY_TYPE_ID );
+
+    public static PostgresTableDefinition ENTITY_SETS =
+            new PostgresTableDefinition( "entity_sets" )
+                    .addColumns( ID, NAME, ENTITY_TYPE_ID, TITLE, DESCRIPTION, CONTACTS )
+                    .setUnique( NAME );
 
     public static PostgresTableDefinition ENTITY_TYPES =
             new PostgresTableDefinition( "entity_types" )
@@ -78,44 +84,6 @@ public final class PostgresTable {
                             SCHEMAS,
                             CATEGORY )
                     .setUnique( NAMESPACE, NAME );
-
-    public static PostgresTableDefinition ENTITY_SETS =
-            new PostgresTableDefinition( "entity_sets" )
-                    .addColumns( ID, NAME, ENTITY_TYPE_ID, TITLE, DESCRIPTION, CONTACTS )
-                    .setUnique( NAME );
-
-    public static PostgresTableDefinition LINKING_VERTICES =
-            new PostgresTableDefinition( "linking_vertices" )
-                    .addColumns( GRAPH_ID, VERTEX_ID, GRAPH_DIAMETER, ENTITY_KEY_IDS )
-                    .primaryKey( GRAPH_ID, VERTEX_ID );
-
-    public static PostgresTableDefinition ASSOCIATION_TYPES =
-            new PostgresTableDefinition( "association_types" )
-                    .addColumns( ID, SRC, DST, BIDIRECTIONAL );
-
-    public static PostgresTableDefinition ORGANIZATIONS =
-            new PostgresTableDefinition( "organizations" )
-                    .addColumns( ID, NULLABLE_TITLE, DESCRIPTION, ALLOWED_EMAIL_DOMAINS, MEMBERS );
-
-    public static PostgresTableDefinition ROLES =
-            new PostgresTableDefinition( "roles" )
-                    .addColumns( ROLE_ID, ORGANIZATION_ID, NULLABLE_TITLE, DESCRIPTION, PRINCIPAL_IDS )
-                    .primaryKey( ROLE_ID, ORGANIZATION_ID );
-
-    public static PostgresTableDefinition ENTITY_SET_PROPERTY_METADATA =
-            new PostgresTableDefinition( "entity_set_property_metadata" )
-                    .addColumns( ENTITY_SET_ID, PROPERTY_TYPE_ID, TITLE, DESCRIPTION, SHOW )
-                    .primaryKey( ENTITY_SET_ID, PROPERTY_TYPE_ID );
-
-    public static PostgresTableDefinition VERTEX_IDS_AFTER_LINKING =
-            new PostgresTableDefinition( "vertex_ids_after_linking" )
-                    .addColumns( GRAPH_ID, VERTEX_ID, NEW_VERTEX_ID )
-                    .primaryKey( GRAPH_ID, VERTEX_ID );
-
-    public static PostgresTableDefinition SCHEMA =
-            new PostgresTableDefinition( "schemas" )
-                    .addColumns( NAMESPACE, NAME_SET )
-                    .primaryKey( NAMESPACE );
 
     public static PostgresTableDefinition ENUM_TYPES =
             new PostgresTableDefinition( "enum_types" )
@@ -136,9 +104,33 @@ public final class PostgresTable {
             new PostgresTableDefinition( "linked_entity_sets" )
                     .addColumns( ID, ENTITY_SET_IDS );
 
-    public static PostgresTableDefinition COMPLEX_TYPES =
-            new PostgresTableDefinition( "complex_types" )
-                    .addColumns( ID, NAMESPACE, NAME, TITLE, DESCRIPTION, PROPERTIES, BASE_TYPE, SCHEMAS, CATEGORY )
+    public static PostgresTableDefinition LINKING_EDGES =
+            new PostgresTableDefinition( "linking_edges" )
+                    .addColumns( GRAPH_ID, SRC_LINKING_VERTEX_ID, EDGE_VALUE, DST_LINKING_VERTEX_ID )
+                    .primaryKey( GRAPH_ID, SRC_LINKING_VERTEX_ID, EDGE_VALUE, DST_LINKING_VERTEX_ID );
+
+    public static PostgresTableDefinition LINKING_VERTICES =
+            new PostgresTableDefinition( "linking_vertices" )
+                    .addColumns( GRAPH_ID, VERTEX_ID, GRAPH_DIAMETER, ENTITY_KEY_IDS )
+                    .primaryKey( GRAPH_ID, VERTEX_ID );
+
+    public static PostgresTableDefinition NAMES =
+            new PostgresTableDefinition( "names" )
+                    .addColumns( SECURABLE_OBJECTID, NAME )
+                    .primaryKey( SECURABLE_OBJECTID );
+
+    public static PostgresTableDefinition ORGANIZATIONS =
+            new PostgresTableDefinition( "organizations" )
+                    .addColumns( ID, NULLABLE_TITLE, DESCRIPTION, ALLOWED_EMAIL_DOMAINS, MEMBERS );
+
+    public static PostgresTableDefinition PERMISSIONS =
+            new PostgresTableDefinition( "permissions" )
+                    .addColumns( ACL_KEY, PRINCIPAL_TYPE, PRINCIPAL_ID, PostgresColumn.PERMISSIONS )
+                    .primaryKey( ACL_KEY, PRINCIPAL_TYPE, PRINCIPAL_ID );
+
+    public static PostgresTableDefinition PROPERTY_TYPES =
+            new PostgresTableDefinition( "property_types" )
+                    .addColumns( ID, NAMESPACE, NAME, DATATYPE, TITLE, DESCRIPTION, SCHEMAS, PII, ANALYZER )
                     .setUnique( NAMESPACE, NAME );
 
     public static PostgresTableDefinition REQUESTS =
@@ -146,11 +138,20 @@ public final class PostgresTable {
                     .addColumns( ACL_KEY, PRINCIPAL_TYPE, PRINCIPAL_ID, PostgresColumn.PERMISSIONS, REASON, STATUS )
                     .primaryKey( ACL_KEY, PRINCIPAL_TYPE, PRINCIPAL_ID );
 
-    public static PostgresTableDefinition EDM_VERSIONS =
-            new PostgresTableDefinition( "edm_versions" )
-                    .addColumns( EDM_VERSION_NAME, EDM_VERSION )
-                    .primaryKey( EDM_VERSION_NAME, EDM_VERSION )
-                    .setUnique( EDM_VERSION_NAME, EDM_VERSION );
+    public static PostgresTableDefinition ROLES =
+            new PostgresTableDefinition( "roles" )
+                    .addColumns( ROLE_ID, ORGANIZATION_ID, NULLABLE_TITLE, DESCRIPTION, PRINCIPAL_IDS )
+                    .primaryKey( ROLE_ID, ORGANIZATION_ID );
+
+    public static PostgresTableDefinition SCHEMA =
+            new PostgresTableDefinition( "schemas" )
+                    .addColumns( NAMESPACE, NAME_SET )
+                    .primaryKey( NAMESPACE );
+
+    public static PostgresTableDefinition SECURABLE_OBJECTS =
+            new PostgresTableDefinition( "securable_objects" )
+                    .addColumns( ACL_KEY, SECURABLE_OBJECT_TYPE )
+                    .primaryKey( ACL_KEY );
 
     public static PostgresTableDefinition SYNC_IDS =
             new PostgresTableDefinition( "sync_ids" )
@@ -158,23 +159,24 @@ public final class PostgresTable {
                     .primaryKey( ENTITY_SET_ID, SYNC_ID )
                     .setUnique( ENTITY_SET_ID, SYNC_ID );
 
-    public static PostgresTableDefinition LINKING_EDGES =
-            new PostgresTableDefinition( "linking_edges" )
-                    .addColumns( GRAPH_ID, SRC_LINKING_VERTEX_ID, EDGE_VALUE, DST_LINKING_VERTEX_ID )
-                    .primaryKey( GRAPH_ID, SRC_LINKING_VERTEX_ID, EDGE_VALUE, DST_LINKING_VERTEX_ID );
+    public static PostgresTableDefinition VERTEX_IDS_AFTER_LINKING =
+            new PostgresTableDefinition( "vertex_ids_after_linking" )
+                    .addColumns( GRAPH_ID, VERTEX_ID, NEW_VERTEX_ID )
+                    .primaryKey( GRAPH_ID, VERTEX_ID );
 
-    public static PostgresTableDefinition AUDIT_LOG =
-            new PostgresTableDefinition( "audit_log" )
-                    .addColumns( ACL_KEY,
-                            EVENT_TYPE,
-                            PRINCIPAL_TYPE,
-                            PRINCIPAL_ID,
-                            TIME_UUID,
-                            AUDIT_ID,
-                            DATA_ID,
-                            BLOCK_ID )
-                    .primaryKey( ACL_KEY, EVENT_TYPE, PRINCIPAL_TYPE, PRINCIPAL_ID, TIME_UUID )
-                    .setUnique( ACL_KEY, EVENT_TYPE, PRINCIPAL_TYPE, PRINCIPAL_ID, TIME_UUID );
+
+    //                case PERMISSIONS:
+    // TODO: Once Cassandra fixes SASI + Collection column inde
+    //            return new CassandraTableBuilder( PERMISSIONS )
+    //                        .ifNotExists()
+    //                        .partitionKey( CommonColumns.ACL_KEYS )
+    //                        .clusteringColumns( PRINCIPAL_TYPE, PRINCIPAL_ID )
+    //                        .columns( CommonColumns.PERMISSIONS )
+    //                        .staticColumns( SECURABLE_OBJECT_TYPE )
+    //                        .secondaryIndex( PRINCIPAL_TYPE,
+    //            PRINCIPAL_ID,
+    //            CommonColumns.PERMISSIONS,
+    //            SECURABLE_OBJECT_TYPE );
 
     private PostgresTable() {
     }
