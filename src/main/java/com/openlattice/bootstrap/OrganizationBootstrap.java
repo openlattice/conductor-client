a5aeaf8f9e8dd41c7a76e3197b81e684ebb2c459
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-11-15 18:39:10 -0800

    Add boostrap code

---
+++ src/main/java/com/openlattice/bootstrap/OrganizationBootstrap.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2017. OpenLattice, Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ */
+
+package com.openlattice.bootstrap;
+
+import static com.openlattice.bootstrap.AuthorizationBootstrap.GLOBAL_ADMIN_ROLE;
+import static com.openlattice.bootstrap.AuthorizationBootstrap.GLOBAL_USER_ROLE;
+import static com.openlattice.bootstrap.AuthorizationBootstrap.OPENLATTICE_ROLE;
+
+import com.dataloom.authorization.Principal;
+import com.dataloom.authorization.PrincipalType;
+import com.dataloom.organization.Organization;
+import com.dataloom.organizations.HazelcastOrganizationService;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSet;
+import java.util.UUID;
+
+/**
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+public class OrganizationBootstrap {
+    public static Organization OPENLATTICE = createOpenLatticeOrg();
+    public static Organization GLOBAL      = createGlobalOrg();
+    private final boolean initialized;
+
+    public OrganizationBootstrap( HazelcastOrganizationService organizationService ) {
+        organizationService.createOrganization( OPENLATTICE_ROLE.getPrincipal(), OPENLATTICE );
+        organizationService.createOrganization( GLOBAL_ADMIN_ROLE.getPrincipal(), GLOBAL );
+        organizationService.addMembers( OPENLATTICE.getId(),
+                ImmutableSet.of(
+                        OPENLATTICE_ROLE.getPrincipal()
+                ) );
+        organizationService.addMembers( OPENLATTICE.getId(),
+                ImmutableSet.of(
+                        GLOBAL_USER_ROLE.getPrincipal(),
+                        GLOBAL_ADMIN_ROLE.getPrincipal()
+                ) );
+        initialized = true;
+    }
+
+    public boolean isInitialized() {
+        return initialized;
+    }
+
+    public static Organization createGlobalOrg() {
+        UUID id = BootstrapConstants.GLOBAL_ORGANIZATION_ID;
+        Principal org = new Principal( PrincipalType.ORGANIZATION, "global" );
+        String title = "Global Organization";
+        return new Organization( Optional.of( id ),
+                org,
+                title,
+                Optional.absent(),
+                ImmutableSet.of(),
+                ImmutableSet.of(),
+                ImmutableSet.of() );
+    }
+
+    public static Organization createOpenLatticeOrg() {
+        UUID id = new UUID( 0, 0 );
+        Principal org = new Principal( PrincipalType.ORGANIZATION, "openlattice" );
+        String title = "OpenLattice, Inc.";
+        return new Organization( Optional.of( id ),
+                org,
+                title,
+                Optional.absent(),
+                ImmutableSet.of(),
+                ImmutableSet.of(),
+                ImmutableSet.of() );
+    }
+}
