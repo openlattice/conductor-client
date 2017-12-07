8e7168ad5cce98452c0734e2c721734de3bef68e
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-12-06 21:18:35 -0800

    Logging

--- src/main/java/com/openlattice/bootstrap/UserBootstrap.java
+++ src/main/java/com/openlattice/bootstrap/UserBootstrap.java
@@ -21,12 +21,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UserBootstrap {
-    private static final Logger logger = LoggerFactory.getLogger(UserBootstrap.class);
+    private static final Logger logger = LoggerFactory.getLogger( UserBootstrap.class );
+
     public UserBootstrap(
             HazelcastInstance hazelcastInstance,
             SecurePrincipalsManager spm,
             DbCredentialService dbCredService ) throws InterruptedException {
-        logger.info("Starting activation of existing auth0 users.");
+        logger.info( "Starting activation of existing auth0 users." );
         IMap<String, Auth0UserBasic> users = hazelcastInstance.getMap( HazelcastMap.USERS.name() );
         IAtomicLong nextTime = hazelcastInstance.getAtomicLong( UserMapstore.class.getCanonicalName() );
         AclKey userRoleAclKey = spm.lookup( AuthorizationBootstrap.GLOBAL_USER_ROLE.getPrincipal() );
@@ -37,7 +38,7 @@ public class UserBootstrap {
             Thread.sleep( 1000 );
         }
 
-        logger.info("Starting processing of {} users.",users.size() );
+        logger.info( "Starting processing of {} users.", users.size() );
 
         for ( Entry<String, Auth0UserBasic> userEntry : users.entrySet() ) {
             String userId = userEntry.getKey();
@@ -48,22 +49,23 @@ public class UserBootstrap {
                 if ( !spm.principalExists( principal ) ) {
                     checkState( user.getUserId().equals( userId ),
                             "Retrieved user id must match submitted user id" );
+                    logger.info( "Activating user {}", userId );
                     dbCredService.createUser( userId );
                     String title = ( user.getNickname() != null && user.getNickname().length() > 0 ) ?
                             user.getNickname() :
                             user.getEmail();
                     spm.createSecurablePrincipalIfNotExists( principal,
                             new SecurablePrincipal( Optional.absent(), principal, title, Optional.absent() ) );
-                }
 
-                AclKey userAclKey = spm.lookup( principal );
+                    AclKey userAclKey = spm.lookup( principal );
 
-                if ( user.getRoles().contains( SystemRole.AUTHENTICATED_USER.getName() ) ) {
-                    spm.addPrincipalToPrincipal( userRoleAclKey, userAclKey );
-                }
+                    if ( user.getRoles().contains( SystemRole.AUTHENTICATED_USER.getName() ) ) {
+                        spm.addPrincipalToPrincipal( userRoleAclKey, userAclKey );
+                    }
 
-                if ( user.getRoles().contains( SystemRole.ADMIN.getName() ) ) {
-                    spm.addPrincipalToPrincipal( adminRoleAclKey, userAclKey );
+                    if ( user.getRoles().contains( SystemRole.ADMIN.getName() ) ) {
+                        spm.addPrincipalToPrincipal( adminRoleAclKey, userAclKey );
+                    }
                 }
             }
         }
