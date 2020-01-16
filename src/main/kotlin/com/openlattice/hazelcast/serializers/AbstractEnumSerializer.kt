1663eb2fb92c129ea3c0af67488876bce8414839
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-01-16 11:57:59 -0800

    Automatically generate enum values and avoid hash lookup

--- src/main/kotlin/com/openlattice/hazelcast/serializers/AbstractEnumSerializer.kt
+++ src/main/kotlin/com/openlattice/hazelcast/serializers/AbstractEnumSerializer.kt
@@ -5,13 +5,13 @@ import com.google.common.cache.CacheLoader
 import com.google.common.cache.LoadingCache
 import com.hazelcast.nio.ObjectDataInput
 import com.hazelcast.nio.ObjectDataOutput
+import org.apache.commons.lang.math.RandomUtils
 
 abstract class AbstractEnumSerializer<T : Enum<T>> : TestableSelfRegisteringStreamSerializer<Enum<T>> {
+    protected val enumArray = enumCache.getOrPut(clazz) { this.clazz.enumConstants as Array<Enum<*>> } as Array<Enum<T>>
 
     companion object {
-        private val enumCache: LoadingCache<Class<*>, Array<*>> = CacheBuilder.newBuilder().build( CacheLoader.from { key ->
-            key!!.enumConstants
-        })
+        private val enumCache: MutableMap<Class<*>, Array<Enum<*>>> = mutableMapOf()
 
         @JvmStatic
         fun serialize(out: ObjectDataOutput, `object`: Enum<*>) {
@@ -21,15 +21,19 @@ abstract class AbstractEnumSerializer<T : Enum<T>> : TestableSelfRegisteringStre
         @JvmStatic
         fun deserialize(targetClass: Class<*>, `in`: ObjectDataInput): Enum<*> {
             val ord = `in`.readInt()
-            return enumCache.get(targetClass)[ord] as Enum<*>
+            return enumCache.getValue(targetClass)[ord]
         }
     }
 
     override fun write(out: ObjectDataOutput, `object`: Enum<T>) {
-        return serialize(out, `object` )
+        return serialize(out, `object`)
     }
 
     override fun read(`in`: ObjectDataInput): Enum<T> {
-        return deserialize( this.clazz, `in` ) as Enum<T>
+        return enumArray[`in`.readInt()]
+    }
+
+    override fun generateTestValue(): Enum<T> {
+        return enumArray[RandomUtils.nextInt(enumArray.size)]
     }
 }
